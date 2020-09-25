304a6e17873361cbdee10a549edb4480dd1224cf
fred-ihu <edm.fred@gmail.com>
2020-09-25 14:25:43 +0200

    strapi install (OK)

---
+++ back-static-blog/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5520be13bb68b840c78e5356a436382e'),
+    },
+  },
+});
---
+++ back-static-blog/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cbb064ef-cdaf-4903-b15f-618ae186a738'
+};
