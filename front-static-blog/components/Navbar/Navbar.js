f2886c33ae8055f2087a037c807ef00963633e68
fred-ihu <edm.fred@gmail.com>
2020-09-28 17:13:32 +0200

    create Navbar component (OK)

---
+++ front-static-blog/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  console.log("Navbar");
+  return (
+    <div className="w-full py-5 lg:space-x-24 > * text-center mx-auto shadow-lg">
+      <Link href="/">
+        <a className="py-2 px-5 rounded bg-transparent hover:bg-green-300 text-black hover:text-white duration-300">
+          Home
+        </a>
+      </Link>
+      <Link href="/categories">
+        <a className="py-2 px-5 rounded bg-transparent hover:bg-green-300 text-black hover:text-white duration-300">
+          Categories
+        </a>
+      </Link>
+      <Link href="/">
+        <a className="py-2 px-5 rounded bg-transparent hover:bg-green-300 text-black hover:text-white duration-300">
+          About
+        </a>
+      </Link>
+
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ front-static-blog/pages/categories.js
@@ -0,0 +1,9 @@
+import Navbar from '../components/Navbar/Navbar';
+
+const Categories = () => {
+  console.log('Categories')
+  return (
+    <div><Navbar /> Categories</div>
+)};
+
+export default Categories;
