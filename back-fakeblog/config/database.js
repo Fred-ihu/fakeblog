683a98cd6a9e52150a92e6d38c38c55a00ec22a8
fred-ihu <edm.fred@gmail.com>
2020-10-12 10:48:02 +0200

    put back the env change in databasejs

--- back-fakeblog/config/database.js
+++ back-fakeblog/config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       "connector": "bookshelf",
       "settings": {
         "client": "postgres",
-        "host": env('DATABASE_HOST'),
-        "port": env('DATABASE_PORT'),
-        "database": env('DATABASE_NAME'),
-        "username": env('DATABASE_USERNAME'),
-        "password": env('DATABASE_PASSWORD'),
+        "host": "${process.env.DATABASE_HOST}",
+        "port": "${process.env.DATABASE_PORT}",
+        "database": "${process.env.DATABASE_NAME}",
+        "username": "${process.env.DATABASE_USERNAME}",
+        "password": "${process.env.DATABASE_PASSWORD}",
         "ssl": { "rejectUnauthorized": false }
       },
       "options": {
@@ -20,22 +20,6 @@ module.exports = ({ env }) => ({
   }
 });
 
-// ! Configuration with SQLite DB
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
 
 
 // module.exports = ({ env }) => ({
@@ -50,6 +34,28 @@ module.exports = ({ env }) => ({
 //         "database": "${process.env.DATABASE_NAME}",
 //         "username": "${process.env.DATABASE_USERNAME}",
 //         "password": "${process.env.DATABASE_PASSWORD}",
+//         "ssl": { "rejectUnauthorized": false }
+//       },
+//       "options": {
+//         "debug": true
+//       }
+//     }
+//   }
+// });
+
+
+// module.exports = ({ env }) => ({
+//   "defaultConnection": "default",
+//   "connections": {
+//     "default": {
+//       "connector": "bookshelf",
+//       "settings": {
+//         "client": "postgres",
+//         "host": env('DATABASE_HOST'),
+//         "port": env('DATABASE_PORT'),
+//         "database": env('DATABASE_NAME'),
+//         "username": env('DATABASE_USERNAME'),
+//         "password": env('DATABASE_PASSWORD'),
 //         "ssl": { "rejectUnauthorized": false }
 //       },
 //       "options": {
