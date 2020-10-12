de268fb6fd502269b6431e1a5f2c309a730e93f0
fred-ihu <edm.fred@gmail.com>
2020-10-12 10:33:09 +0200

    Try to change env in databasejs on config back-fakeblog file

--- back-fakeblog/config/database.js
+++ back-fakeblog/config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       "connector": "bookshelf",
       "settings": {
         "client": "postgres",
-        "host": "${process.env.DATABASE_HOST}",
-        "port": "${process.env.DATABASE_PORT}",
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
+        "host": env('DATABASE_HOST'),
+        "port": env('DATABASE_PORT'),
+        "database": env('DATABASE_NAME'),
+        "username": env('DATABASE_USERNAME'),
+        "password": env('DATABASE_PASSWORD'),
         "ssl": { "rejectUnauthorized": false }
       },
       "options": {
@@ -35,4 +35,26 @@ module.exports = ({ env }) => ({
 //       },
 //     },
 //   },
+// });
+
+
+// module.exports = ({ env }) => ({
+//   "defaultConnection": "default",
+//   "connections": {
+//     "default": {
+//       "connector": "bookshelf",
+//       "settings": {
+//         "client": "postgres",
+//         "host": "${process.env.DATABASE_HOST}",
+//         "port": "${process.env.DATABASE_PORT}",
+//         "database": "${process.env.DATABASE_NAME}",
+//         "username": "${process.env.DATABASE_USERNAME}",
+//         "password": "${process.env.DATABASE_PASSWORD}",
+//         "ssl": { "rejectUnauthorized": false }
+//       },
+//       "options": {
+//         "debug": true
+//       }
+//     }
+//   }
 // });
